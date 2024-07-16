0c393cbba49b4b2f615c418533cce838adf7691e
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2024-07-16 19:09:39 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,17 @@
+// let x = 7;
+// if (x=="7") {
+// 	console.log("They are the same");
+
+// }
+// else{
+// 	console.log("They aren't the same");
+// }
+// if (x==="7") {
+// 	console.log("They are the same");
+
+// }
+// else{
+// 	console.log("They aren't the same");
+// }
+//window.prompt("Enter a number :")
+alert("hello")
